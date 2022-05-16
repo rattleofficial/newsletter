9adff823ca9d7a4592c34d5dd79b03a787f2fcad
rattleofficial <102285779+rattleofficial@users.noreply.github.com>
2022-05-16 19:26:17 +0530

    Add files via upload

---
+++ newsletter.py
@@ -0,0 +1,25 @@
+import streamlit as sl
+import pyrebase
+config={
+    "apiKey": "AIzaSyCilEt1u-MY3Yz56_fPoGbsysgLoweReYI",
+    "authDomain": "newsletter-f9a5f.firebaseapp.com",
+    "projectId": "newsletter-f9a5f",
+    "databaseURL":"https://newsletter-f9a5f-default-rtdb.firebaseio.com/",
+    "storageBucket": "newsletter-f9a5f.appspot.com",
+    "messagingSenderId": "793080417319",
+    "appId": "1:793080417319:web:4bcc987850a3fd934c5202",
+    "measurementId": "G-NNWFG7Z232"
+}
+firebase=pyrebase.initialize_app(config)
+db=firebase.database()
+sl.title('Join our newsletter now!')
+sl.markdown("Hello join our Rattlefoundation's newsletter to get our updates upto date through your email.Rattlefoundation is a tech provider trusted by 1000+ people around the world.")
+inp=sl.text_input('Your name:')
+inp2=sl.text_input('Your email:')
+if sl.button('Join!'):
+    data={inp:True}
+    s=inp2.replace('.','')
+    data2={s:True}
+    db.child("Name").set(data)
+    db.child("Email id").set(data2)
+    m=sl.header('Thank you for joining our newsletter!')
